89996fd3e3b2dd0d7ff9af203d464a975ee17a03
davidszatmari <szatmaridavid7@gmail.com>
2023-09-13 18:57:16 +0200

    +javascript

---
+++ app.cetralhome/script.js
@@ -0,0 +1,10 @@
+// JavaScript functions to open and close the popup
+function openPopup() {
+    var popup = document.getElementById("popup");
+    popup.style.display = "block";
+  }
+
+  function closePopup() {
+    var popup = document.getElementById("popup");
+    popup.style.display = "none";
+  }
